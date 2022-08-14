6bf10a66fcadd7e2fa4a6006bae0928d4991b8c1
kst164 <kartheek@tammana.org>
2022-08-14 19:11:17 +0530

    Create example

--- examples/gen_and_solve.rs
+++ examples/gen_and_solve.rs
@@ -6,22 +6,12 @@ use maze_traits::MazeGenerator;
 use maze_traits::MazeSolver;
 
 fn main() {
-    let rows = 1000;
-    let cols = 1000;
+    let rows = 15;
+    let cols = 15;
 
     let mut m = SquareGrid::new(rows, cols);
 
-    /*for i in 0..3 {
-        m.remove_wall(&(i, 0), &(i + 1, 0));
-    }
-    for i in 0..3 {
-        m.remove_wall(&(3, i), &(3, i + 1));
-    }
-    m.remove_wall(&(1, 0), &(1, 1));*/
-
     DepthFirstSearch::new().generate(&mut m, &(0, 0), &(rows - 1, cols - 1));
-
-    //println!("{}", m);
 
     let keys = BreadthFirstSearch::new().solve(&m, &(0, 0), &(rows - 1, cols - 1));
 
